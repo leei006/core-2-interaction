63e0912476a3106cb8e16f9119bc1453477d125b
leei006 <leei006@newschool.edu>
2024-04-11 01:02:49 -0400

    finalizing website

---
+++ studio/project 2.2/script.js
@@ -0,0 +1,347 @@
+
+let collection = [
+{
+    "name": "Braised Beef in Soy Sauce",
+    "korean": "Jangjorim 장조림",
+    "image": "img/braisedbeef.jpg",
+    "color": "Brown",
+    "genre": "Protein",
+    "temp": "Cold, Hot",
+    "spice": "Not Spicy",
+    "link": "https://www.maangchi.com/recipe/jangjorim"
+},
+{
+    "name": "Braised Black Cod with Radish",
+    "korean": "Eundaegu-jorim 은대구조림",
+    "image": "img/braisedcod.jpg",
+    "color": "Red",
+    "genre": "Sea Food",
+    "temp": "Hot",
+    "spice": "Mild",
+    "link": "https://www.maangchi.com/recipe/eundaegu-jorim"
+},
+{
+    "name": "Braised Lotus Root",
+    "korean": "Yeon-geun-jorim 연근조림",
+    "image": "img/braisedlotusroot.jpg",
+    "color": "Brown",
+    "genre": "Root",
+    "temp": "Cold",
+    "spice": "Not Spicy",
+    "link": "https://www.maangchi.com/recipe/yeon-geun-jorim"
+},
+{
+    "name": "Broccoli with Tofu",
+    "korean": "Broccoli dubu-muchim 브로콜리 두부무침",
+    "image": "img/broccolitofu.jpg",
+    "color": "Green",
+    "genre": "Hearty Veggies",
+    "temp": "Cold",
+    "spice": "Not Spicy",
+    "link": "https://www.maangchi.com/recipe/broccoli-dubu-muchim"
+},
+{
+    "name": "Cabbage Pancake",
+    "korean": "Baechujeon 배추전",
+    "image": "img/cabbagepancake.jpg",
+    "color": "Brown",
+    "genre": "Hearty Veggies",
+    "temp": "Hot",
+    "spice": "Not Spicy",
+    "link": "https://www.maangchi.com/recipe/baechujeon"
+},
+{
+    "name": "Dried Shredded Pollock",
+    "korean": "Bugeopo gochujang-muchim 북어포 고추장무침",
+    "image": "img/driedpollock.jpg",
+    "color": "Beige",
+    "genre": "Sea Food",
+    "temp": "Cold",
+    "spice": "Mild",
+    "link": "https://www.maangchi.com/recipe/bugeopo-gochujang-muchim"
+},
+{
+    "name": "Eggs in Soy Broth",
+    "korean": "Gyeran-jangjorim 계란장조림",
+    "image": "img/soysauceggs.png",
+    "color": "Dark Brown",
+    "genre": "Protein",
+    "temp": "Cold",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/gyeran-jangjorim"
+},
+{
+    "name": "Green Onion Kimchi",
+    "korean": "Pa-kimchi 파김치",
+    "image": "img/",
+    "color": "Green",
+    "genre": "Frail Greens",
+    "temp": "Cold",
+    "spice": "Mild",
+    "link":"https://www.maangchi.com/recipe/pa-kimchi"
+},
+{
+    "name": "Kimchi Pancake",
+    "korean": "Kimchijeon 김치전",
+    "image": "img/",
+    "color": "Red",
+    "genre": "Hearty Veggies",
+    "temp": "Hot",
+    "spice": "Mild",
+    "link":"https://www.maangchi.com/recipe/kimchijeon"
+},
+{
+    "name": "Minari",
+    "korean": "Minari-muchim 미나리무침",
+    "image": "img/",
+    "color": "Green",
+    "genre": "Root",
+    "temp": "Cold",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/minari-muchim"
+},
+{
+    "name": "Mung Bean Pancake",
+    "korean": "Bindaetteok 빈대떡",
+    "image": "img/",
+    "color": "Yellow",
+    "genre": "Frail Greens",
+    "temp": "Hot",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/bindaetteok"
+},
+{
+    "name": "Mung Bean Sprout",
+    "korean": "Sukjunamul-muchim 숙주나물무침",
+    "image": "img/",
+    "color": "Yellow",
+    "genre": "Frail Greens",
+    "temp": "Cold",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/sukjunamul-muchim"
+},
+{
+    "name": "Mustard Greens Kimchi",
+    "korean": "Gat-kimchi 갓김치",
+    "image": "img/",
+    "color": "Green",
+    "genre": "Frail Greens",
+    "temp": "Cold",
+    "spice": "Mild",
+    "link":"https://www.maangchi.com/recipe/gat-kimchi"
+},
+{
+    "name": "Perilla Leaf Pancake",
+    "korean": "Kkaennip-jangtteok 깻잎장떡",
+    "image": "img/",
+    "color": "Green",
+    "genre": "Frail Greens",
+    "temp": "Hot",
+    "spice": "Mild",
+    "link":"https://www.maangchi.com/recipe/kkaennip-jangtteok"
+},
+{
+    "name": "Seafood Pancake",
+    "korean": "Haemul-pajeon 해물파전",
+    "image": "img/",
+    "color": "Yellow",
+    "genre": ["Frail Greens","Sea Food"],
+    "temp": "Hot",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/haemul-pajeon"
+},
+{
+    "name": "Shrimp Pancake",
+    "korean": "Saeujeon 새우전",
+    "image": "img/",
+    "color": "Yellow",
+    "genre": "Protein",
+    "temp": "Hot",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/saeujeon"
+},
+{
+    "name": "Potato Pancake",
+    "korean": "Gamjajeon 감자전",
+    "image": "img/",
+    "color": "Brown",
+    "genre": "Hearty Veggies",
+    "temp": "Hot",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/simple-gamjajeon"
+},
+{
+    "name": "Braised Potatoes",
+    "korean": "Maeun-gamja-jorim 매운감자조림",
+    "image": "img/",
+    "color": "Red",
+    "genre": "Hearty Veggies",
+    "temp": "Cold",
+    "spice": "Spicy",
+    "link":"https://www.maangchi.com/recipe/maeun-gamja-jorim"
+},
+{
+    "name": "Cucumber Kimchi",
+    "korean": "Oi-muchim 오이무침",
+    "image": "img/",
+    "color": "Green",
+    "genre": "Hearty Veggies",
+    "temp": "Cold",
+    "spice": "Spicy",
+    "link":"https://www.maangchi.com/recipe/oi-muchim"
+},
+{
+    "name": "Raw Marinated Crab",
+    "korean": "Yangnyeom-gejang 양념게장",
+    "image": "img/",
+    "color": "Red",
+    "genre": "Sea Food",
+    "temp": "Cold",
+    "spice": "Spicy",
+    "link":"https://www.maangchi.com/recipe/yangnyeom-gejang"
+},
+{
+    "name": "Stir-Fried Blood Sausage",
+    "korean": "Sundae Bokkeum 순대볶음",
+    "image": "img/",
+    "color": "Brown",
+    "genre": "Protein",
+    "temp": "Hot",
+    "spice": "Spicy",
+    "link":"https://www.maangchi.com/recipe/sundae-bokkeum"
+},
+{
+    "name": "Stir Fried Octopus",
+    "korean": "Nakji-Bokkeum 낙지볶음",
+    "image": "img/",
+    "color": "Red",
+    "genre": "Sea Food",
+    "temp": "Hot",
+    "spice": "Spicy",
+    "link":"https://www.maangchi.com/recipe/nakji-bokkeum"
+},
+{
+    "name": "Stir-Fried Fernbrake",
+    "korean": "Gosari-namul 고사리나물",
+    "image": "img/",
+    "color": "Brown",
+    "genre": "Frail Greens",
+    "temp": "Cold, Hot",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/gosari-namul"
+},
+{
+    "name": "Stir-Fried Oyster Mushrooms",
+    "korean": "Neutari-beoseot-bokkeum 느타리버섯볶음",
+    "image": "img/",
+    "color": "Brown",
+    "genre": "Hearty Veggies",
+    "temp": "Cold, Hot",
+    "spice": "Not Spicy",
+    "link":"https://www.maangchi.com/recipe/neutari-beoseot-bokkeum"
+}]
+
+document.addEventListener("DOMContentLoaded", function(){
+    const recipes = document.getElementById("recipes");
+    const colors = document.querySelectorAll(".content3 [recipe-id]");
+    const genres = document.querySelectorAll(".content3 [genre-id]");
+    const spice = document.querySelectorAll(".content3 [spice-id]");
+    const temp = document.querySelectorAll(".content3 [temp-id]");
+
+    colors.forEach(colorElement => {
+        colorElement.addEventListener("click", function(){
+            const dishColor = colorElement.getAttribute("recipe-id");
+
+            const matchingDishes = collection.filter(w => w.color.toLowerCase() === dishColor.toLowerCase());
+
+            if (matchingDishes.length > 0) {
+                displayRecipes(matchingDishes);
+            } else {
+                recipes.innerHTML = "No dishes found for this color.";
+            }
+        });
+    });
+
+
+    genres.forEach(genreElement => {
+        genreElement.addEventListener("click", function(){
+            const genreId = genreElement.getAttribute("genre-id").toLowerCase();
+            console.log("Clicked genre:", genreId); 
+
+            const matchingDishes = collection.filter(w => {
+                if (typeof w.genre === 'string') {
+                    return w.genre.toLowerCase() === genreId;
+                } else if (Array.isArray(w.genre)) {
+                    return w.genre.map(g => g.toLowerCase()).includes(genreId);
+                }
+                return false;
+            });
+
+            if (matchingDishes.length > 0) {
+                displayRecipes(matchingDishes);
+            } else {
+                recipes.innerHTML = "No dishes found for this genre.";
+            }
+        });
+    });
+
+
+    spice.forEach(spiceElement => {
+        spiceElement.addEventListener("click", function(){
+            const spiceId = spiceElement.getAttribute("spice-id").toLowerCase();
+            
+            const matchingDishes = collection.filter(w => w.spice.toLowerCase() === spiceId.toLowerCase());
+         
+        if (matchingDishes.length > 0) {
+            displayRecipes(matchingDishes);
+        } else {
+            recipes.innerHTML = "No dishes found for this spice"
+        }
+    });
+});
+
+    temp.forEach(tempElement => {
+        tempElement.addEventListener("click", function(){
+        const tempId = tempElement.getAttribute("temp-id").toLowerCase();
+
+        const matchingDishes = collection.filter(w => w.temp.toLowerCase() === tempId.toLowerCase())
+
+        if (matchingDishes.length > 0) {
+            displayRecipes(matchingDishes);
+        } else {
+            recipes.innerHTML = "No dishes found for this temp"
+        }
+        });
+    });
+
+
+    function displayRecipes(dishes) {
+        let dataHTML = "";
+        dishes.forEach(dish => {
+            dataHTML += `
+                <button type="button" class="collapsible"><p class="colltitle">${dish.name}</p></button>
+                <div class="content">
+                    <p>${dish.korean}</p>
+                    <img class="collimage" src="${dish.image}">
+                    <a href=${dish.link} class="link">Link to recipe</a>
+                </div>
+            `;
+        });
+        recipes.innerHTML = dataHTML;
+
+        var coll = document.getElementsByClassName("collapsible");
+        for (let i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+            });
+        }
+    }
+});
+
+
